00859673f056f67320cb5b92e3d1adc1da2f9f70
Rob Gage <rmg@oakleycomputing.org>
2025-09-30 20:01:29 -0400

    Add test structures to pups_core

---
+++ core/tests/master.rs
@@ -0,0 +1,91 @@
+// Copyright Rob Gage 2025
+
+use pups_core::{
+    Input,
+    prelude::*,
+    Mode
+};
+
+
+#[derive(Clone, Eq, PartialEq)]
+enum TestItem {
+    A (String),
+    B (usize),
+    C (isize),
+}
+
+impl Parser<TestInput> for TestItem {
+
+    type Output = TestItem;
+
+    type Error = TestError;
+
+    type Message = ();
+
+    fn apply<_Mode: Mode>(
+        &self,
+        input: &mut TestInput
+    ) -> ParseResult<TestItem, TestError, (), _Mode> {
+        let cursor: usize = input.cursor();
+        let Some(item) = input.next() else {
+            input.set_cursor(cursor);
+            return ParseResult::Failure (
+                _Mode::convert_error(TestError {
+                    encountered_item: None,
+                    expected_item: self.clone(),
+                    position: input.cursor
+                }),
+                _Mode::new_message_container()
+            )
+        };
+        if item == *self {
+            ParseResult::Success(
+                _Mode::convert_output(item),
+                _Mode::new_message_container()
+            )
+        } else {
+            input.set_cursor(cursor);
+            ParseResult::Failure (
+                _Mode::convert_error(TestError {
+                    encountered_item: Some(item),
+                    expected_item: self.clone(),
+                    position: input.cursor
+                }),
+                _Mode::new_message_container()
+            )
+        }
+    }
+
+}
+
+
+struct TestError {
+    encountered_item: Option<TestItem>,
+    expected_item: TestItem,
+    position: usize,
+}
+
+
+struct TestInput {
+    items: Vec<TestItem>,
+    cursor: usize,
+}
+
+impl Input for TestInput {
+    type Item = TestItem;
+
+    fn cursor(&self) -> usize { self.cursor }
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let item: TestItem = self.peek()?;
+        self.cursor += 1;
+        Some(item)
+    }
+
+    fn peek(&self) -> Option<Self::Item> {
+        self.items.get(self.cursor).map(|item| item.clone())
+    }
+
+    fn set_cursor(&mut self, position: usize) { self.cursor = position }
+
+}
--- library/src/lib.rs
+++ library/src/lib.rs
@@ -1,4 +1,5 @@
 // Copyright Rob Gage 2025
 
 pub use pups_core::prelude::*;
+
 pub use pups_text::prelude::*;
