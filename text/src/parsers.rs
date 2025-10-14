dddf9de0ff133a3f275ccf2c72171ed12e72a070
Rob Gage <rmg@oakleycomputing.org>
2025-10-14 17:25:11 -0400

    Create `number` text parser

--- text/src/parsers.rs
+++ text/src/parsers.rs
@@ -1,6 +1,7 @@
 // Copyright Rob Gage 2025
 
 mod newline;
+mod number;
 mod token;
 mod whitespace;
 mod unicode_identifier;
@@ -14,6 +15,7 @@ use pups_core::{
     Parser,
 };
 use newline::Newline;
+use number::Number;
 use token::Token;
 use whitespace::Whitespace;
 use crate::parsers::unicode_identifier::UnicodeIdentifier;
@@ -25,6 +27,13 @@ where
     I: Input<'a, Item = C> + TextInput,
 { Newline }
 
+/// Parses a number composed of ASCII decimal digits 0-9
+pub const fn number<'a, C, I>(
+) -> impl Parser<'a, I, Output = I::Slice, Error = (), Message = ()>
+where
+    C: Character,
+    I: Input<'a, Item = C> + TextInput,
+{ Number }
 
 /// Parses a given lexeme from text
 pub const fn token<'a, I>(
@@ -34,7 +43,6 @@ where
     I: Input<'a> + TextInput,
 { Token (lexeme) }
 
-
 /// Parses a unicode identifier
 pub const fn unicode_identifier<'a, C, I>(
 ) -> impl Parser<'a, I, Output = I::Slice, Error = (), Message = ()>
@@ -42,7 +50,6 @@ where
     C: Character,
     I: Input<'a, Item = C> + TextInput,
 { UnicodeIdentifier }
-
 
 /// Parses whitespace
 pub const fn whitespace<'a, C, I>(
---
+++ text/src/parsers/number.rs
@@ -0,0 +1,53 @@
+// Copyright Rob Gage 2025
+
+use crate::{
+    Character,
+    TextInput
+};
+use pups_core::{
+    Input,
+    Mode,
+    ModeResult::{
+        self,
+        Failure,
+        Success,
+    },
+    Parser
+};
+
+/// Parses a number
+pub struct Number;
+
+impl<'a, C, I> Parser<'a, I> for Number
+where
+    C: Character,
+    I: Input<'a, Item = C> + TextInput,
+{
+    type Output = I::Slice;
+
+    type Error = ();
+
+    type Message = ();
+
+    fn apply<_Mode: Mode>(
+        &self,
+        input: &'a I
+    ) -> ModeResult<I::Slice , (), (), _Mode> {
+        let start: usize = input.save_cursor();
+        loop {
+            let cursor: usize = input.save_cursor();
+            let Some (character) = input.next() else { break };
+            if !character.is_ascii_decimal() { input.restore_cursor(cursor); break }
+        }
+        let length: usize = input.save_cursor() - start;
+        if length > 0 {
+            input.restore_cursor(start);
+            Success (
+                _Mode::convert_output(input.consume(length).unwrap()),
+                _Mode::new_message_container()
+            )
+        }
+        else { Failure (_Mode::convert_error(()), _Mode::new_message_container())}
+    }
+
+}
