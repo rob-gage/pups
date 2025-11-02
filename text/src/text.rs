c8eac99663d71941c63d7f013472e57fc7de7e26
Rob Gage <rmg@oakleycomputing.org>
2025-11-01 22:45:03 -0400

    Remove undefined behavior in `Text` by adding `UnsafeCell`

--- text/src/text.rs
+++ text/src/text.rs
@@ -5,21 +5,24 @@ use crate::{
     TextInput,
 };
 use pups_core::Input;
-use std::marker::PhantomData;
+use std::{
+    cell::UnsafeCell,
+    marker::PhantomData,
+};
 
 /// UTF-8 text that can be consumed by parsers
 pub struct Text {
     /// The buffer that stores the `Text`
     buffer: String,
     /// The byte offset in the buffer that represents the start of the `Text`
-    byte_offset: usize,
+    byte_offset: UnsafeCell<usize>,
 }
 
 impl<'a> Text {
 
     /// Creates a new `Text` from a `&str`
     pub fn from_string(string: &'a str) -> Self {
-        Self { buffer: string.to_string(), byte_offset: 0 }
+        Self { buffer: string.to_string(), byte_offset: UnsafeCell::new(0) }
     }
 
 }
@@ -32,38 +35,30 @@ impl<'a> Input<'a> for Text {
 
     fn advance(&self) {
         if let Some (character) = self.peek() {
-            unsafe {
-                let mutable: *mut Self = self as *const Self as *mut Self;
-                (*mutable).byte_offset += character.length();
-            }
+            unsafe { *self.byte_offset.get() += character.length() };
         }
     }
 
-    fn peek(&self) -> Option<Self::Item> { self.buffer[self.byte_offset..].chars().next() }
+    fn peek(&self) -> Option<Self::Item> {
+        let byte_offset: usize = unsafe { *self.byte_offset.get() };
+        self.buffer[byte_offset..].chars().next()
+    }
 
     fn slice(&'a self, start: usize, end: usize) -> &'a str { &self.buffer[start..end] }
 
-    fn move_cursor(&self, cursor: usize) {
-        unsafe {
-            let mutable: *mut Self = self as *const Self as *mut Self;
-            (*mutable).byte_offset = cursor;
-        }
-    }
+    fn move_cursor(&self, cursor: usize) { unsafe { *self.byte_offset.get() = cursor } }
 
-    fn store_cursor(&self) -> usize { self.byte_offset }
+    fn store_cursor(&self) -> usize { unsafe { *self.byte_offset.get() } }
 
 }
 
 impl<'a> TextInput for Text {
 
-    fn starts_with(&self, string: &str) -> bool
-    { self.buffer[self.byte_offset..].starts_with(string) }
-
-    fn skip_bytes(&self, byte_count: usize) {
-        unsafe {
-            let mutable: *mut Self = self as *const Self as *mut Self;
-            (*mutable).byte_offset += byte_count;
-        }
+    fn starts_with(&self, string: &str) -> bool {
+        let byte_offset: usize = unsafe { *self.byte_offset.get() };
+        self.buffer[byte_offset..].starts_with(string)
     }
+
+    fn skip_bytes(&self, byte_count: usize) { unsafe { *self.byte_offset.get() += byte_count; } }
 
 }
