51d7587f63ffcf89810be39cbef613c89039a199
Rob Gage <rmg@oakleycomputing.org>
2025-11-21 21:57:00 -0500

    Create `to_result` method on `ModeResult`

--- core/src/mode_result.rs
+++ core/src/mode_result.rs
@@ -3,26 +3,33 @@
 use crate::Mode;
 
 /// A result produced by applying a parser to input
-pub enum ModeResult<Output, Error, Message, _Mode>
+pub enum ModeResult<O, E, M, _Mode>
 where
     _Mode: Mode,
 {
     /// Represents a successful parser application
-    Success (_Mode::OutputForm<Output>, _Mode::MessageContainer<Message>),
+    Success (_Mode::OutputForm<O>, _Mode::MessageContainer<M>),
     /// Represents a failed parser application
-    Failure (_Mode::ErrorForm<Error>, _Mode::MessageContainer<Message>),
+    Failure (_Mode::ErrorForm<E>, _Mode::MessageContainer<M>),
 }
-
 
 impl<Output, Error, Message, _Mode> ModeResult<Output, Error, Message, _Mode>
 where
     _Mode: Mode
 {
 
-    /// Returns `true` if this `ParseResult` represents a successful parser application
+    /// Returns `true` if this result represents a successful parser application
     pub const fn is_success(&self) -> bool { matches!(self, ModeResult::Success(..)) }
 
-    /// Returns `true` if this `ParseResult` represents a failed parser application
+    /// Returns `true` if this result represents a failed parser application
     pub const fn is_failure(&self) -> bool { matches!(self, ModeResult::Failure(..)) }
+
+    /// Returns this `ModeResult` as a `Result`
+    pub fn to_result(self) -> Result<_Mode::OutputForm<Output>, _Mode::ErrorForm<Error>> {
+        match self {
+            ModeResult::Success (output, _) => Ok (output),
+            ModeResult::Failure (error, _) => Err (error),
+        }
+    }
 
 }
